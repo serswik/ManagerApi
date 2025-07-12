4a5950191369e2186dac84e408067a71996a668d
serswik <megasergik@icloud.com>
2025-07-12 14:25:12 +0300

    Created controllers folder and added usercontroller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ManagerApi.DTOs;
+using ManagerApi.Models;
+
+namespace ManagerApi.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private static readonly List<User> Users = new();
+        private static int _nextId = 1;
+    }
+}
